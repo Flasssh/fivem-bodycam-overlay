0a46c564b106b532ef99c0ffccc4f880363d18a1
Romain Antunes <doxerfx@gmail.com>
2021-04-19 21:17:46 +0200

    Ajout footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import ReactTooltip from 'react-tooltip';
 
 import { BodyCam } from './components/BodyCam';
 import { BodyCamConfigurator } from './components/BodyCamConfigurator';
+import { Footer } from './components/Footer/Footer';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         </div>
         <BodyCamConfigurator />
       </div>
+      <Footer />
       <ReactTooltip />
     </div>
   );
